f15e83f9d2355895ccc50616462b77af21e80086
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-08-18 00:03:21 -0400

    Add new features: add_days and days_diff_with
    This features were added to the DateAssistant class.

--- date_assistant/core.py
+++ date_assistant/core.py
@@ -1,9 +1,24 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+
 class DateAssistant:
     def __init__(self, date: str):
         self.date = date
+        self._date = datetime.strptime(date, '%Y-%m-%d')
 
     def __repr__(self):
         return f'DateAssistant({self.date!r})'
 
     def __str__(self):
         return self.date
+
+    def add_days(self, num_days: int) -> str:
+        self._date += relativedelta(days=num_days)
+        self.date = self._date.strftime('%Y-%m-%d')
+        return self.date
+
+    def days_diff_with(self, date_to_compare: str) -> int:
+        date_to_compare = datetime.strptime(date_to_compare, '%Y-%m-%d')
+        return abs(self._date - date_to_compare).days
--- tests/test_date_assistant.py
+++ tests/test_date_assistant.py
@@ -18,3 +18,35 @@ def test_date_assistant_repr(example_date):
 
 def test_date_assistant_str(example_date):
     assert str(example_date) == '2021-01-01'
+
+
+@pytest.mark.parametrize(
+    'num_days, expected_result',
+    [
+        (-1, '2020-12-31'),
+        (31, '2021-02-01'),
+        (365, '2022-01-01'),
+        (-366, '2020-01-01'),
+        (0, '2021-01-01'),
+    ],
+)
+def test_date_assistant_add_days(example_date, num_days, expected_result):
+    response = example_date.add_days(num_days)
+    assert response == expected_result
+
+
+@pytest.mark.parametrize(
+    'date_to_compare, expected_result',
+    [
+        ('2020-12-31', 1),
+        ('2021-02-01', 31),
+        ('2022-01-01', 365),
+        ('2020-01-01', 366),
+        ('2021-01-01', 0),
+    ],
+)
+def test_date_assistant_days_diff_with(
+    example_date, date_to_compare, expected_result
+):
+    response = example_date.days_diff_with(date_to_compare)
+    assert response == expected_result
